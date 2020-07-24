db33534161a26ec5cf0be13780f4151ea7e5bb3f
UnicornsOnLSD <44349936+UnicornsOnLSD@users.noreply.github.com>
2020-07-24 23:15:31 +0100

    Wrote the function for actually getting the torrents

---
+++ functions/index.js
@@ -0,0 +1,14 @@
+const functions = require('firebase-functions');
+const TorrentSearchApi = require('torrent-search-api');
+
+
+TorrentSearchApi.enableProvider('1337x');
+TorrentSearchApi.enableProvider('Rarbg');
+
+exports.searchAll = functions.https.onCall((data, context) => {
+    const search = data.search;
+    return TorrentSearchApi.search(search).then((results) => {
+        console.log(results);
+        return results;
+    });
+})
